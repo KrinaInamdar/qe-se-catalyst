f15e8c99eeb4bc878fed2636748ed62e5c3e4bd4
james.koch <james.koch@slalom.com>
2025-12-03 17:08:15 -0500

    added all feats

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
+import { Outlet } from "react-router-dom";
+
 function App() {
-  return (
-    <div>
-      <h1>QE SE Catalyst</h1>
-      <p>Start building your React application here.</p>
-    </div>
-  );
+  return <Outlet />;
 }
 
 export default App;
